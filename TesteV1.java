1710e10ee372be65018e3288f99229790ca2b6d7
Letícia Almeida <leticiaalmeidadev@gmail.com>
2026-03-26 17:41:04 -0300

    learn(ap008): implementa método rediensiona único

--- TesteV1.java
+++ TesteV1.java
@@ -5,8 +5,19 @@ public class TesteV1 {
         var v = new VetorDinamico ();
         while (true) {
             var valor = gerador.nextInt (1, 11);
-            v.adicionar (valor);
+            var oQueFazer = gerador.nextInt (0, 2);
+            switch (oQueFazer) {
+                case 0:
+                    System.out.println ("Adicionando...");
+                    v.adicionar (valor);
+                    break;
+                case 1:
+                    System.out.println ("Removendo...");
+                    v.remover ();
+                    break;
+            }
             System.out.println (v);
+            System.out.println ("========================");
             Thread.sleep(5000);
         }
     }   
--- VetorDinamico.java
+++ VetorDinamico.java
@@ -2,23 +2,33 @@ public class VetorDinamico {
     private int quantidade;
     private int capacidade;
     private int [] elementos;
+    private static final int CAP_MINIMA = 4;
 
     public VetorDinamico () {
         quantidade = 0;
-        capacidade = 4;
+        capacidade = CAP_MINIMA;
         elementos = new int [capacidade];
     }
 
     public void adicionar (int e) {
         if (estaCheio()) {
-            aumentarCapacidade();
+            redimensionar (2);
         }
         elementos [quantidade] = e;
         quantidade++;
     }
 
-    public void aumentarCapacidade () {
-        var novo = new int [capacidade * 2];
+    public void remover () {
+        if (!estaVazio()) {
+            quantidade--;
+            if (capacidade >  CAP_MINIMA && quantidade == capacidade / 4) {
+                redimensionar (0.5);
+            }
+        }
+    }
+
+    private void redimensionar (double fator) {
+        var novo = new int [(int)(capacidade * fator)];
         for (int i = 0; i < quantidade; i++) {
             novo[i] = elementos[i];
         }
